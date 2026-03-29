0e5d7307a99d781869e6b3c003e581966987181b
balajiaatish-cloud <balajiaatish@gmail.com>
2026-03-29 08:13:52 +0530

    Delete game directory

--- game/run.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-echo Starting Hill Climb Racer Game Server...
-echo.
-echo Opening browser at http://localhost:8000
-echo.
-echo Close this window to stop the server.
-echo.
-python -m http.server 8000
